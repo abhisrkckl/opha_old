747b314a0948f49c02da1914d31f864e66584ed5
Abhimanyu S <abhisrkckl@gmail.com>
2020-12-31 18:56:18 +0530

    --

--- scripts/analysis.py
+++ scripts/analysis.py
@@ -6,10 +6,10 @@ import corner
 import pickle
 
  
-model, prior_transform, lnlike_gauss, lnlike_kde = ModelSetup.setup_model('Spin', '../data/config/outburst_numbers.txt', '../data/config/spin_priors.txt', 0.306, 'A', 'gauss')
-resultA_spin = nestle.sample(lnlike_kde, prior_transform, model.N_PARAMS, npoints=100, method='multi', callback=nestle.print_progress)
-with open('resultA_spin_samples.dat','wb') as outfile:
-    pickle.dump(resultA_spin, outfile)
+#model, prior_transform, lnlike_gauss, lnlike_kde = ModelSetup.setup_model('Spin', '../data/config/outburst_numbers.txt', '../data/config/spin_priors.txt', 0.306, 'A', 'gauss')
+#resultA_spin = nestle.sample(lnlike_kde, prior_transform, model.N_PARAMS, npoints=100, method='multi', callback=nestle.print_progress)
+#with open('resultA_spin_samples.dat','wb') as outfile:
+#    pickle.dump(resultA_spin, outfile)
 
 #model, prior_transform, lnlike_gauss, lnlike_kde = ModelSetup.setup_model('Spin', '../data/config/outburst_numbers.txt', '../data/config/spin_priors.txt', 0.306, 'B', 'gauss')
 #resultB_spin = nestle.sample(lnlike_kde, prior_transform, model.N_PARAMS, npoints=100, method='multi', callback=nestle.print_progress)
@@ -17,13 +17,13 @@ with open('resultA_spin_samples.dat','wb') as outfile:
 #    pickle.dump(resultB_spin, outfile)
 
 
-#model, prior_transform, lnlike_gauss, lnlike_kde = ModelSetup.setup_model('NoSpin', '../data/config/outburst_numbers.txt', '../data/config/nospin_priors.txt', 0.306, 'A', 'gauss')
-#resultA_nospin = nestle.sample(lnlike_kde, prior_transform, model.N_PARAMS, npoints=100, method='multi', callback=nestle.print_progress)
-#with open('resultA_nospin_samples.dat','wb') as outfile:
-#    pickle.dump(resultA_nospin, outfile)
+model, prior_transform, lnlike_gauss, lnlike_kde = ModelSetup.setup_model('NoSpin', '../data/config/outburst_numbers.txt', '../data/config/nospin_priors.txt', 0.306, 'A', 'gauss')
+resultA_nospin = nestle.sample(lnlike_kde, prior_transform, model.N_PARAMS, npoints=100, method='multi', callback=nestle.print_progress)
+with open('resultA_nospin_samples.dat','wb') as outfile:
+    pickle.dump(resultA_nospin, outfile)
 
-#model, prior_transform, lnlike_gauss, lnlike_kde = ModelSetup.setup_model('NoSpin', '../data/config/outburst_numbers.txt', '../data/config/nospin_priors.txt', 0.306, 'B', 'gauss')
-#resultB_nospin = nestle.sample(lnlike_kde, prior_transform, model.N_PARAMS, npoints=100, method='multi', callback=nestle.print_progress)
-#with open('resultB_nospin_samples.dat','wb') as outfile:
-#    pickle.dump(resultB_nospin, outfile)
+model, prior_transform, lnlike_gauss, lnlike_kde = ModelSetup.setup_model('NoSpin', '../data/config/outburst_numbers.txt', '../data/config/nospin_priors.txt', 0.306, 'B', 'gauss')
+resultB_nospin = nestle.sample(lnlike_kde, prior_transform, model.N_PARAMS, npoints=100, method='multi', callback=nestle.print_progress)
+with open('resultB_nospin_samples.dat','wb') as outfile:
+    pickle.dump(resultB_nospin, outfile)
 
