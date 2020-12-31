d3dc4d067a1cb139941894db351f6a99cc7ffa15
Abhimanyu S <abhisrkckl@gmail.com>
2020-12-31 18:57:18 +0530

    Updated lightcurve_model.py

--- scripts/lightcurve/lightcurve_model.py
+++ scripts/lightcurve/lightcurve_model.py
@@ -1,5 +1,6 @@
 from __future__ import division
 import numpy as np
+from scipy.special import log_ndtr
 
 def model_fn(x0,y0):
     def templ(x):
@@ -15,6 +16,7 @@ def model_fn(x0,y0):
     
     return model
 
+""" 
 def lnlike_fn(x0,y0,x1,y1,kill=False):
     model = model_fn(x0,y0)
     
@@ -24,7 +26,30 @@ def lnlike_fn(x0,y0,x1,y1,kill=False):
         return -sum((y_pred-y1)**2)/2/err**2 - len(y1)*np.log(err) if not (outq and kill) else -np.inf
     
     return lnlike    
+ """
 
+def lnlike_fn(template, lightcurve_det, lightcurve_cen, kill=False):
+    model = model_fn(template.t, template.M)
+    
+    def lnlike(params):
+        outq_det, y_pred_det = model(lightcurve_det.t, params[:-1])
+        outq_cen, y_pred_cen = model(lightcurve_cen.t, params[:-1])
+        
+        err = params[-1]
+        
+        res_det = (lightcurve_det.M - y_pred_det)/err
+        res_cen = (lightcurve_cen.M - y_pred_cen)/err
+        
+        if outq_det and outq_cen and kill:
+            return -np.inf
+        else:
+            lnlike_det = -sum(res_det**2)/2 - len(res_det)*np.log(err)
+            lnlike_cen = sum(log_ndtr(res_cen))
+            
+            return lnlike_det + lnlike_cen
+    
+    return lnlike
+ 
 def prior_transform_fn(params_min, params_max):
     spans = np.array(params_max)-np.array(params_min)
     
